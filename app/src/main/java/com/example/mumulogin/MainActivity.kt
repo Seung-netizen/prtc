48ff0a11529c53186beb248394f725472e2c573f
Seung-netizen <juioman76@gmail.com>
2026-07-21 10:01:59 +0900

    feat: 화면 이동

---
+++ app/src/main/java/com/example/mumulogin/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mumulogin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mumulogin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        nextScreen()
+    }
+    private fun nextScreen(){
+        binding.layoutMo.setOnClickListener {
+            val intent = Intent(this, MoInfo::class.java)
+            startActivity(intent)
+        }
+    }
+}

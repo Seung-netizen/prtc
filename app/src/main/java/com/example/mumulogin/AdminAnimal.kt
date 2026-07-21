992ec900eddb21c329cfd128deb49c8b1b3b929d
Seung-netizen <juioman76@gmail.com>
2026-07-21 14:21:57 +0900

    feat: 동물 프로필 관리

--- app/src/main/java/com/example/mumulogin/AdminAnimal.kt
+++ app/src/main/java/com/example/mumulogin/AdminAnimal.kt
@@ -1,36 +1,20 @@
 package com.example.mumulogin
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.mumulogin.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class AdminAnimal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_admin_animal)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        nextScreen()
-    }
-    private fun nextScreen(){
-        binding.layoutMo.setOnClickListener {
-            val intent = Intent(this, MoInfo::class.java)
-            startActivity(intent)
-        }
-        binding.layoutAn.setOnClickListener {
-            val intent = Intent(this, AdminAnimal::class.java)
         }
     }
 }
